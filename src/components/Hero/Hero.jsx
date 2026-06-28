4f0538c0abf4f5c54316ea7e49507a14593145cc
Pohi11 <60932567+Pohi11@users.noreply.github.com>
2026-06-28 15:00:04 -0400

    updatee

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ export const Hero = () => {
         <h1 className={styles.title}>Hi, I'm Jonathan</h1>
         <p className={styles.description}>
           I'm a software engineer at CGI and a Carleton University Computer Science graduate. I'm interested in Artificial Intelligence, Cybersecurity, Automation, and Cloud
-          Architecture. Keep reading to learn more!
+          Architecture.
         </p>
         <div className={styles.buttonContainer}>
           <a href="#contact" className={styles.contactBtn}>
